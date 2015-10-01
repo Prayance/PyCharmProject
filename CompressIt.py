40d03369a59f16137374061ac802905c0e67f0c5
Prayance <up629097@myport.ac.uk>
2015-10-01 14:51:54 +0100

    get elements, get count of filelist

---
+++ CompressIt.py
@@ -0,0 +1,107 @@
+#!/us/bin/python
+import zipfile
+import sys
+
+__author__ = 'elly'
+
+
+def mycompression():
+    try:
+        import zlib
+        compression = zipfile.ZIP_DEFLATED
+    except:
+        compression = zipfile.ZIP_STORED
+
+    modes = {zipfile.ZIP_DEFLATED: 'delated',
+             zipfile.ZIP_STORED: 'stored',
+             }
+
+    print "creating archive"
+    zf = zipfile.ZipFile("/home/elly/sqlBackups/mySecondZip.zip", mode='w')
+    print 'Raw size: ', sys.getsizeof('/home/elly/sqlBackups/01102015_105631/EllyTest.sql')
+    try:
+        print 'adding ellytest.sql with compression mode', modes[compression]
+        zf.write('/home/elly/sqlBackups/01102015_105631/EllyTest.sql', compress_type=compression)
+    finally:
+        print 'closing'
+        zf.close()
+    print '9 compressed size:', sys.getsizeof(zf)
+
+
+def main():
+    mycompression()
+
+
+main()
+'''
+mypath = '/home/elly/sqlBackups/01102015_105631/'
+myfile = mypath + 'EllyTest.sql'
+print 'Raw size: ',sys.getsizeof(myfile)
+with open(myfile, 'rb') as f_in, gzip.open(myfile + '.gz', 'wb') as f_out:
+    shutil.copyfileobj(f_in, f_out)
+
+assert isinstance(f_out, object)
+print '9 compressed size:',sys.getsizeof(f_out)
+
+
+
+def reset():
+    tar = tarfile.open(".", "test.tar.gz", "w:gz")
+    tar.add("/home/elly/sqlBackups/01102015_105631/EllyTest.sql")
+    tar.close()
+
+
+def archive_log():
+    src = '/home/elly/sqlBackups/01102015_121958/EllyTest.sql'
+    dst = '/home/elly/sqlBackups/EllyTest.sql.gz'
+    zip = zipfile.ZipFile(dst, 'w')
+    zip.write(src, os.path.basename(src), zipfile.ZIP_DEFLATED)
+    zip.close()
+    '''
+
+'''
+def reset():
+    tar = tarfile.open('/home/elly/sqlBackups/sample.tar.gz', "w:gz")
+    tar.add('/home/elly/sqlBackups/01102015_105631/EllyTest.sql', filter=reset)
+    tar.close()
+
+
+def myzips():
+    mypath = '/home/elly/sqlBackups/01102015_121958/'
+    zip = zipfile.ZipFile('EllyTest.sql', 'w')
+    zip.write(mypath, os.path.basename(mypath), zipfile.ZIP_DEFLATED)
+    zip.close()
+    print "Job done!"
+
+
+
+def main():
+    reset()
+
+    main()
+      '''
+'''
+myfile = open('/home/elly/sqlBackups/01102015_105631/EllyTest.sql', 'r').read()
+compressed = base64.b64encode(zlib.compress(myfile, 9))
+
+print compressed
+
+output = open('encodecomp.txt', 'a')
+output.write(compressed)
+output.close()
+
+readFile = open('encodecomp.txt', 'r').read
+
+decompressed = zlib.decompress(base64.b64decode(readFile))
+
+
+print 'Raw size: ',sys.getsizeof(myfile)
+
+compressed = zlib.compress(myfile, 9)
+print '9 compressed size:',sys.getsizeof(compressed)
+
+savecomp = open('compdata.txt', 'a')
+savecomp.write(compressed)
+savecomp.close()
+
+decompressed = zlib.decompress(compressed)'''
---
+++ MySQLDBBackup.py
@@ -0,0 +1,57 @@
+#!/us/bin/python
+import os
+import time
+
+__author__ = 'elly'
+
+
+# the mysql database details
+DB_Host = 'localhost'
+DB_User = 'root'
+DB_User_Password = '1234'
+DB_Name = 'EllyTest'
+Backup_Path = '/home/elly/sqlBackups/'
+
+# get current date/time to name the backup
+Current_Datetime = time.strftime('%d%m%Y_%H%M%S')
+
+FinalPath = Backup_Path + Current_Datetime
+
+# check if backup folder already exists and create it if not
+if not os.path.exists(FinalPath):
+    os.makedirs(FinalPath)
+
+# Check if you want to take single database backup or assinged multiple backups in DB_NAME.
+print "checking for databases names file."
+if os.path.exists(DB_Name):
+    file1 = open(DB_Name)
+    multi = 1
+    print "Databases file found..."
+    print "Starting backup of all dbs listed in file " + DB_Name
+else:
+    print "Databases file not found..."
+    print "Starting backup of database " + DB_Name
+    multi = 0
+
+# Starting actual database backup process.
+if multi:
+    in_file = open(DB_Name, "r")
+    flength = len(in_file.readlines())
+    in_file.close()
+    p = 1
+    dbfile = open(DB_Name, "r")
+
+    while p <= flength:
+        db = dbfile.readline()  # reading database name from file
+        db = db[:-1]  # deletes extra line
+        dumpcmd = "mysqldump -u " + DB_User + " -p" + DB_User_Password + " -h " + DB_Host + " " + db + " > " + FinalPath + "/" + db + ".sql"
+        os.system(dumpcmd)
+        p += 1
+    dbfile.close()
+else:
+    db = DB_Name
+    dumpcmd = "mysqldump -u " + DB_User + " -p" + DB_User_Password + " " + db + " > " + FinalPath + "/" + db + ".sql"
+    os.system(dumpcmd)
+
+print "Backup script completed"
+print "Your backups has been created in '" + FinalPath + "' directory"
